eb50936a8cbf951ff47c630eeeec0e44ddadc100
Rohith Joshua M <146416123+RohithJoshua31@users.noreply.github.com>
2026-07-02 14:05:32 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,45 @@
+"""
+utils.py
 
+Utility functions used across the project.
+"""
+
+import os
+import cv2
+
+
+def create_directory(path):
+    """
+    Create directory if it does not exist.
+    """
+    os.makedirs(path, exist_ok=True)
+
+
+def image_exists(image_path):
+    """
+    Check whether an image exists.
+    """
+    return os.path.isfile(image_path)
+
+
+def load_image(image_path):
+    """
+    Load an image using OpenCV.
+    """
+    return cv2.imread(image_path)
+
+
+def show_image(window_name, image):
+    """
+    Display an image.
+    """
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def save_image(output_path, image):
+    """
+    Save an image.
+    """
+    cv2.imwrite(output_path, image)
